08f41ce0e73c22f678ff63c6a52407ae91aa4cb8
yoouyeon <jyeon.yoon59@gmail.com>
2025-03-22 00:03:56 +0900

    💡 프로그래머스 42584 - 주식가격
    스택으로 푸는 방법을 추가함

--- Programmers/Level2/42584_주식가격.js
+++ Programmers/Level2/42584_주식가격.js
@@ -5,7 +5,8 @@
 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/42584
 */
 
-function solution(prices) {
+// 브루트포스 - O(N^2)
+function solution_brute_force(prices) {
   const answer = Array(prices.length).fill(0);
 
   for (let idx = 0; idx < prices.length - 1; idx++) {
@@ -15,6 +16,28 @@ function solution(prices) {
         break;
       }
     }
+  }
+
+  return answer;
+}
+
+// 스택 - O(N)
+function solution_stack(prices) {
+  const answer = Array(prices.length).fill(0);
+  const idxStack = []; // 가격이 떨어지지 않은 것들의 인덱스를 저장한다.
+
+  for (let idx = 0; idx < prices.length; idx++) {
+    while (idxStack.length && prices[idx] < prices[idxStack.at(-1)]) {
+      const top = idxStack.pop();
+      answer[top] = idx - top;
+    }
+    idxStack.push(idx);
+  }
+
+  // 가격이 계속 떨어지지 않은 것들이 남아있다면 답에 반영해준다.
+  while (idxStack.length) {
+    const top = idxStack.pop();
+    answer[top] = prices.length - top - 1;
   }
 
   return answer;
