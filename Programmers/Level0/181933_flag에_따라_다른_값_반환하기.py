bce89ef338183fa62a959090c0b50295542fd634
yoouyeon <jyeon.yoon59@gmail.com>
2025-03-28 00:38:08 +0900

    💡 프로그래머스 181933 - flag에 따라 다른 값 반환하기

---
+++ Programmers/Level0/181933_flag에_따라_다른_값_반환하기.py
@@ -0,0 +1,9 @@
+'''
+⭐️ 문제 정보 ⭐️
+문제 : 181933 - flag에 따라 다른 값 반환하기
+레벨 : Unknown
+링크 : https://school.programmers.co.kr/learn/courses/30/lessons/181933
+'''
+
+def solution(a, b, flag):
+    return a + b if flag else a - b
