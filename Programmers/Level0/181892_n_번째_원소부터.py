9ce611bcc3143bb409ebc679f25199d87ecb6da6
yoouyeon <jyeon.yoon59@gmail.com>
2025-03-28 00:47:03 +0900

    💡 프로그래머스 181892 - n 번째 원소부터

---
+++ Programmers/Level0/181892_n_번째_원소부터.py
@@ -0,0 +1,9 @@
+'''
+⭐️ 문제 정보 ⭐️
+문제 : 181892 - n 번째 원소부터
+레벨 : Level 0
+링크 : https://school.programmers.co.kr/learn/courses/30/lessons/181892
+'''
+
+def solution(num_list, n):
+    return num_list[n - 1:]
