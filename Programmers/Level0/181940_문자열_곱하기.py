c6a80c1340ee5e4374be3ddd3a87e0cbda732813
yoouyeon <jyeon.yoon59@gmail.com>
2025-03-28 00:35:57 +0900

    💡 프로그래머스 181940 - 문자열 곱하기

---
+++ Programmers/Level0/181940_문자열_곱하기.py
@@ -0,0 +1,9 @@
+'''
+⭐️ 문제 정보 ⭐️
+문제 : 181940 - 문자열 곱하기
+레벨 : Level 0
+링크 : https://school.programmers.co.kr/learn/courses/30/lessons/181940
+'''
+
+def solution(my_string, k):
+    return my_string * k
