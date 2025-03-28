943013aac4aa87f9e41362f416290b67b46ee54c
yoouyeon <jyeon.yoon59@gmail.com>
2025-03-28 09:55:30 +0900

    💡 프로그래머스 181937 - n의 배수

---
+++ Programmers/Level0/181937_n의_배수.py
@@ -0,0 +1,9 @@
+'''
+⭐️ 문제 정보 ⭐️
+문제 : 181937 - n의 배수
+레벨 : Level 0
+링크 : https://school.programmers.co.kr/learn/courses/30/lessons/181937
+'''
+
+def solution(num, n):
+    return int(num % n == 0)
