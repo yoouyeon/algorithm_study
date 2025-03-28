5164524a701d03cc478f153f04e8fc07850541ca
yoouyeon <jyeon.yoon59@gmail.com>
2025-03-28 10:00:51 +0900

    💡 프로그래머스 181941 - 문자 리스트를 문자열로 변환하기

---
+++ Programmers/Level0/181941_문자_리스트를_문자열로_변환하기.py
@@ -0,0 +1,9 @@
+'''
+⭐️ 문제 정보 ⭐️
+문제 : 181941 - 문자 리스트를 문자열로 변환하기
+레벨 : Level 0
+링크 : https://school.programmers.co.kr/learn/courses/30/lessons/181941
+'''
+
+def solution(arr):
+    return ''.join(arr)
