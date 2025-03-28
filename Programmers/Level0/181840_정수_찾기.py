2ae19aa5a948a0faa4f160d04578ac63d69834f5
yoouyeon <jyeon.yoon59@gmail.com>
2025-03-28 19:37:40 +0900

    💡 프로그래머스 181840 - 정수 찾기

---
+++ Programmers/Level0/181840_정수_찾기.py
@@ -0,0 +1,9 @@
+'''
+⭐️ 문제 정보 ⭐️
+문제 : 181840 - 정수 찾기
+레벨 : Level 0
+링크 : https://school.programmers.co.kr/learn/courses/30/lessons/181840
+'''
+
+def solution(num_list, n):
+    return int(n in num_list)
