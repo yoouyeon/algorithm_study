4736478bae56cc834f956fb94ee3610c30904f58
yoouyeon <jyeon.yoon59@gmail.com>
2025-03-28 10:04:00 +0900

    💡 프로그래머스 181889 - n 번째 원소까지

---
+++ Programmers/Level0/181889_n_번째_원소까지.py
@@ -0,0 +1,9 @@
+'''
+⭐️ 문제 정보 ⭐️
+문제 : 181889 - n 번째 원소까지
+레벨 : Level 0
+링크 : https://school.programmers.co.kr/learn/courses/30/lessons/181889
+'''
+
+def solution(num_list, n):
+    return num_list[:n]
