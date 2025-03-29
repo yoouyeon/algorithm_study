a0ffab3e0c9747e669a73388a4cc530e5e4bf064
yoouyeon <jyeon.yoon59@gmail.com>
2025-03-29 10:01:58 +0900

    💡 프로그래머스 181843 - 부분 문자열인지 확인하기

---
+++ Programmers/Level0/181843_부분_문자열인지_확인하기.py
@@ -0,0 +1,10 @@
+'''
+⭐️ 문제 정보 ⭐️
+문제 : 181843 - 부분 문자열인지 확인하기
+레벨 : Level 0
+링크 : https://school.programmers.co.kr/learn/courses/30/lessons/181843
+'''
+
+def solution(my_string, target):
+    return int(target in my_string)
+
