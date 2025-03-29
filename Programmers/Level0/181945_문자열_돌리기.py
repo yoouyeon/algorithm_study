06d50987eb73c890866b2097c26311db43246ec1
yoouyeon <jyeon.yoon59@gmail.com>
2025-03-29 10:55:33 +0900

    💡 프로그래머스 181945 - 문자열 돌리기

---
+++ Programmers/Level0/181945_문자열_돌리기.py
@@ -0,0 +1,13 @@
+'''
+⭐️ 문제 정보 ⭐️
+문제 : 181945 - 문자열 돌리기
+레벨 : Level 0
+링크 : https://school.programmers.co.kr/learn/courses/30/lessons/181945
+'''
+
+str = input()
+
+# for char in str:
+#     print(char)
+
+print(*str, sep='\n')
