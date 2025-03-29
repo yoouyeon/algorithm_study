a4f70e9f001134cd4bef8a04466579d217effbbd
yoouyeon <jyeon.yoon59@gmail.com>
2025-03-29 14:01:27 +0900

    💡 프로그래머스 120817 - 배열의 평균값

---
+++ Programmers/Level0/120817_배열의_평균값.py
@@ -0,0 +1,11 @@
+'''
+⭐️ 문제 정보 ⭐️
+문제 : 120817 - 배열의 평균값
+레벨 : Unknown
+링크 : https://school.programmers.co.kr/learn/courses/30/lessons/120817
+'''
+
+import statistics
+
+def solution(numbers):
+    return statistics.mean(numbers)
