7fb89f20d6c64bbcebe7aa5ce800352607f8a907
yoouyeon <jyeon.yoon59@gmail.com>
2025-03-29 10:46:29 +0900

    💡 프로그래머스 181947 - 덧셈식 출력하기

---
+++ Programmers/Level0/181947_덧셈식_출력하기.py
@@ -0,0 +1,9 @@
+''' 
+⭐️ 문제 정보 ⭐️
+문제 : 181947 - 덧셈식 출력하기
+레벨 : Level 0
+링크 : https://school.programmers.co.kr/learn/courses/30/lessons/181947
+'''
+
+a, b = map(int, input().strip().split(' '))
+print(f'{a} + {b} = {a + b}')
