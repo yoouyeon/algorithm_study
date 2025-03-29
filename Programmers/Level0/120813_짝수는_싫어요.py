2ebe3da2365c3c76579406d47308494f91e16d1b
yoouyeon <jyeon.yoon59@gmail.com>
2025-03-29 13:20:35 +0900

    💡 프로그래머스 120813 - 짝수는 싫어요

---
+++ Programmers/Level0/120813_짝수는_싫어요.py
@@ -0,0 +1,10 @@
+'''
+⭐️ 문제 정보 ⭐️
+문제 : 120813 - 짝수는 싫어요
+레벨 : Level 0
+링크 : https://school.programmers.co.kr/learn/courses/30/lessons/120813
+'''
+
+def solution(n):
+    return [x for x in range(1, n + 1, 2)]
+
