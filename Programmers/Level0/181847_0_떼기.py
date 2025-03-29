3cc47053ee72daecda37299b01d23265f3552bb9
yoouyeon <jyeon.yoon59@gmail.com>
2025-03-29 10:24:20 +0900

    💡 프로그래머스 181847 - 0 떼기

---
+++ Programmers/Level0/181847_0_떼기.py
@@ -0,0 +1,9 @@
+''' 
+⭐️ 문제 정보 ⭐️
+문제 : 181847 - 0 떼기
+레벨 : Level 0
+링크 : https://school.programmers.co.kr/learn/courses/30/lessons/181847
+'''
+
+def solution(n_str):
+    return n_str.lstrip('0') 
