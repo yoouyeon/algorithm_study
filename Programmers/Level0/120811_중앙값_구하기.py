a4e1ec10033b06404a36d981266bf9bcaff7db95
yoouyeon <jyeon.yoon59@gmail.com>
2025-03-29 14:07:28 +0900

    💡 프로그래머스 120811 - 중앙값 구하기

---
+++ Programmers/Level0/120811_중앙값_구하기.py
@@ -0,0 +1,11 @@
+'''
+⭐️ 문제 정보 ⭐️
+문제 : 120811 - 중앙값 구하기
+레벨 : Level 0
+링크 : https://school.programmers.co.kr/learn/courses/30/lessons/120811
+'''
+
+import statistics
+
+def solution(array):
+    return statistics.median(array)
