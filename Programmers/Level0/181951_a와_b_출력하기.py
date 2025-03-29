ba9047da5c84b0fb44af8dd99e64c8cc2e0d2db7
yoouyeon <jyeon.yoon59@gmail.com>
2025-03-29 10:29:34 +0900

    💡 프로그래머스 181951 - a와 b 출력하기

---
+++ Programmers/Level0/181951_a와_b_출력하기.py
@@ -0,0 +1,11 @@
+''' 
+⭐️ 문제 정보 ⭐️
+문제 : 181951 - a와 b 출력하기
+레벨 : Level 0
+링크 : https://school.programmers.co.kr/learn/courses/30/lessons/181951
+'''
+
+a, b = map(int, input().strip().split(' '))
+
+print(f"a = {a}")
+print(f"b = {b}")
