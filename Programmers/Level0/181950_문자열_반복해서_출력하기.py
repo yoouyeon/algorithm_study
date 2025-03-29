0a7516e1564a79a9ed26ed9246c01b42749d80dc
yoouyeon <jyeon.yoon59@gmail.com>
2025-03-29 10:30:58 +0900

    💡 프로그래머스 181950 - 문자열 반복해서 출력하기

---
+++ Programmers/Level0/181950_문자열_반복해서_출력하기.py
@@ -0,0 +1,11 @@
+''' 
+⭐️ 문제 정보 ⭐️
+문제 : 181950 - 문자열 반복해서 출력하기
+레벨 : Level 0
+링크 : https://school.programmers.co.kr/learn/courses/30/lessons/181950
+'''
+
+str, n = input().strip().split(' ')
+
+n = int(n)
+print(str * n)
