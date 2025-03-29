ea4a3fb3cd186d17e12d3bb937713a9fa4a31003
yoouyeon <jyeon.yoon59@gmail.com>
2025-03-29 13:55:23 +0900

    💡 프로그래머스 120816 - 피자 나눠 먹기 (3)

---
+++ Programmers/Level0/120816_피자_나눠_먹기_(3).py
@@ -0,0 +1,11 @@
+'''
+⭐️ 문제 정보 ⭐️
+문제 : 120816 - 피자 나눠 먹기 (3)
+레벨 : Level 0
+링크 : https://school.programmers.co.kr/learn/courses/30/lessons/120816
+'''
+
+import math
+
+def solution(slice, n):
+    return math.ceil(n / slice)
