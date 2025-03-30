8209e957d49dbebc7c6ca21a2b63d3f222f9ce34
yoouyeon <jyeon.yoon59@gmail.com>
2025-03-30 15:46:03 +0900

    💡 프로그래머스 120821 - 배열 뒤집기

---
+++ Programmers/Level0/120821_배열_뒤집기.py
@@ -0,0 +1,10 @@
+'''
+⭐️ 문제 정보 ⭐️
+문제 : 120821 - 배열 뒤집기
+레벨 : Level 0
+링크 : https://school.programmers.co.kr/learn/courses/30/lessons/120821
+'''
+
+def solution(num_list):
+    answer = list(reversed(num_list))
+    return answer
