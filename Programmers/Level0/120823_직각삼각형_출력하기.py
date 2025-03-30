87bb45a596bf5a2d59559355209bba3ce580cab4
yoouyeon <jyeon.yoon59@gmail.com>
2025-03-30 15:48:26 +0900

    💡 프로그래머스 120823 - 직각삼각형 출력하기

---
+++ Programmers/Level0/120823_직각삼각형_출력하기.py
@@ -0,0 +1,10 @@
+'''
+⭐️ 문제 정보 ⭐️
+문제 : 120823 - 직각삼각형 출력하기
+레벨 : Level 0
+링크 : https://school.programmers.co.kr/learn/courses/30/lessons/120823
+'''
+
+n = int(input())
+for bumnyang in range(1, n + 1):
+    print('*' * (bumnyang))
