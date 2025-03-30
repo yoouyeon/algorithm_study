638504c800cfe44a4444dfb2d86b7a11c7f9a5c4
yoouyeon <jyeon.yoon59@gmail.com>
2025-03-30 15:47:05 +0900

    💡 프로그래머스 120822 - 뒤집힌 문자열

---
+++ Programmers/Level0/120822_뒤집힌_문자열.py
@@ -0,0 +1,17 @@
+'''
+⭐️ 문제 정보 ⭐️
+문제 : 120822 - 뒤집힌 문자열
+레벨 : Level 0
+링크 : https://school.programmers.co.kr/learn/courses/30/lessons/120822
+'''
+
+def solution(my_string):
+    answer = my_string[::-1]
+    return answer
+
+    # 1. 문자열을 배열로 만들기
+    # arr = list(my_string)
+    # 2. 뒤집는다.
+    # arr.reverse()
+    # 3. 문자열로 만들어준다.
+    # answer = ''.join(arr)
