4dfea2f7443f3bf5f1bc4da0d3fbb1cfe2974dcd
yoouyeon <jyeon.yoon59@gmail.com>
2025-04-07 08:52:16 +0900

    💡 리트코드 70 - Climbing Stairs
    주석 수정

--- Leetcode/70.climbing-stairs.ts
+++ Leetcode/70.climbing-stairs.ts
@@ -3,7 +3,7 @@
 /**
  * [Idea]
  * Dynamic programming
- * 현재 계단에 도달하기 위해서는 이전 계단에서 1칸 올라오거나 2칸 올라와야 한다.
+ * 현재 계단에 도달하기 위해서는 1칸 전 계단에서 1칸 올라오거나 2칸 전 계단에서 2칸 올라와야 한다.
  * memo[i] = memo[i - 1] + memo[i - 2] 로 계산할 수 있다.
  *
  * [Time Complexity]
@@ -13,7 +13,6 @@
  * [Space Complexity]
  * O(n) (n: 계단의 개수)
  * memo 배열을 n + 1 크기로 생성한다.
- *
  */
 function climbStairs(n: number): number {
   const memo = new Array<number>(n + 1).fill(0);
