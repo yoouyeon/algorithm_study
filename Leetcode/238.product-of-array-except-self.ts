439fd512b92b4ca52e9f626aae5770ccaeee2dc3
yoouyeon <jyeon.yoon59@gmail.com>
2025-04-08 10:28:28 +0900

    💡 리트코드 238 - Product of Array Except Self
    주석 추가

--- Leetcode/238.product-of-array-except-self.ts
+++ Leetcode/238.product-of-array-except-self.ts
@@ -2,10 +2,24 @@
 
 /**
  * [Idea]
+ * nums[i] 를 기준으로 왼쪽 부분 (prefix)은 계속 구간을 늘려주고, 오른쪽 부분(suffix)은 계속 구간을 줄여주는 방식
+ * i = 0:
+ *      [ ] * [2, 3, 4, 5, 6] => prefix: 0, suffix: 2 * 3 * 4 * 5 * 6
+ * i = 1:
+ *      [1] * [3, 4, 5, 6] => prefix: 1, suffix: 3 * 4 * 5 * 6
+ * i = 2:
+ *      [1, 2] * [4, 5, 6] => prefix: 1 * 2, suffix: 4 * 5 * 6
+ * i = 3:
+ *      [1, 2, 3] * [5, 6] => prefix: 1 * 2 * 3, suffix: 5 * 6
  *
  * [Time Complexity]
+ * O(n) (n: nums의 길이)
+ * 1. prefix를 구하는 과정에서 O(n)
+ * 2. suffix를 구하는 과정에서 O(n)
  *
  * [Space Complexity]
+ * O(1)
+ * answer 배열을 제외한 추가적인 공간을 사용하지 않음
  *
  */
 function productExceptSelf(nums: number[]): number[] {
