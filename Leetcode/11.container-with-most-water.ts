38f16a5e6be75f05134dcb70c7d9a4bcc6b4cb54
yoouyeon <jyeon.yoon59@gmail.com>
2025-05-06 23:57:28 +0900

    💡 리트코드 11 - Container With Most Water

---
+++ Leetcode/11.container-with-most-water.ts
@@ -0,0 +1,29 @@
+/*
+ * [11] Container With Most Water
+ */
+
+function maxArea(height: number[]): number {
+  const n = height.length;
+  let answer = 0;
+  let leftIdx = 0;
+  let rightIdx = n - 1;
+
+  while (leftIdx < rightIdx) {
+    const minHeight = Math.min(height[leftIdx], height[rightIdx]);
+    const area = (rightIdx - leftIdx) * minHeight;
+
+    answer = Math.max(answer, area);
+
+    // 전략: 항상 더 낮은 높이의 포인터를 이동 (높이 증가 가능성 최대화)
+    // 높이가 낮은 것을 옮겨야 최소 높이 상승을 기대할 수 있다.
+    // 높은 것을 옮기면 운 좋으면 minHeight 유지, 운 나쁘면 minHeight가 더 작아진다.
+
+    if (height[leftIdx] === minHeight) {
+      leftIdx++;
+      continue;
+    }
+    rightIdx--;
+  }
+
+  return answer;
+}
