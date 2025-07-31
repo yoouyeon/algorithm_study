837dba7e0473060c6effb7f5ce5057d75f4c3d7a
yoouyeon <jyeon.yoon59@gmail.com>
2025-08-01 00:02:42 +0900

    💡 리트코드 121 - Best Time to Buy and Sell Stock

---
+++ Leetcode/121.best-time-to-buy-and-sell-stock.ts
@@ -0,0 +1,15 @@
+/*
+ * [121] Best Time to Buy and Sell Stock
+ */
+// prices 배열로 주어진 날들 중에서 사고 팔았을 때 얻을 수 있는 가장 큰 이익 구하기
+function maxProfit(prices: number[]): number {
+  let minPrice = Infinity;
+  let maxProfit = 0;
+
+  for (const price of prices) {
+    minPrice = Math.min(minPrice, price);
+    maxProfit = Math.max(maxProfit, price - minPrice);
+  }
+
+  return maxProfit;
+}
