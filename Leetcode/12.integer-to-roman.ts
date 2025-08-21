a7491a80046d55737e6453a0060fc99a09006468
yoouyeon <jyeon.yoon59@gmail.com>
2025-08-21 10:23:14 +0900

    💡 리트코드 12 - 코드 리팩토링

--- Leetcode/12.integer-to-roman.ts
+++ Leetcode/12.integer-to-roman.ts
@@ -1,8 +1,41 @@
 /*
  * [12] Integer to Roman
  */
+// greedy
+function intToRoman1(num: number): string {
+  const symbols = [
+    "M",
+    "CM",
+    "D",
+    "CD",
+    "C",
+    "XC",
+    "L",
+    "XL",
+    "X",
+    "IX",
+    "V",
+    "IV",
+    "I",
+  ];
+  const values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
 
-function intToRoman(num: number): string {
+  let answer: string[] = [];
+
+  for (let idx = 0; idx < values.length; idx++) {
+    const value = values[idx];
+    const cnt = Math.floor(num / value);
+    if (cnt <= 0) continue;
+    const symbol = symbols[idx];
+    answer.push(symbol.repeat(cnt));
+    num %= value;
+  }
+
+  return answer.join("");
+}
+
+// map과 iterator를 이용해서 비교적 덜 명료했던 첫번째 코드
+function intToRoman2(num: number): string {
   const symbolTable = new Map([
     [1000, "M"],
     [900, "CM"],
