de5b8ac9814aaf804afb2e2a94a235d1e6fd257f
yoouyeon <jyeon.yoon59@gmail.com>
2025-10-03 20:52:01 +0900

    💡 프로그래머스 120821 - 배열 뒤집기

---
+++ Programmers/Level0/120821_배열_뒤집기.js
@@ -0,0 +1,10 @@
+/* 
+⭐️ 문제 정보 ⭐️
+문제 : 120821 - 배열 뒤집기
+레벨 : Level 0
+링크 : https://school.programmers.co.kr/learn/courses/30/lessons/120821
+*/
+
+function solution(num_list) {
+  return num_list.reverse();
+}
