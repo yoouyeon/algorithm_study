ed2a6a9e2f216a2800bc4f4bbeade2732146b171
yoouyeon <jyeon.yoon59@gmail.com>
2025-10-03 21:32:04 +0900

    💡 프로그래머스 87390 - n^2 배열 자르기

--- Programmers/Level2/87390_n^2_배열_자르기.js
+++ Programmers/Level2/87390_n^2_배열_자르기.js
@@ -1,4 +1,27 @@
-function solution(n, left, right) {
+/* 
+⭐️ 문제 정보 ⭐️
+문제 : 87390 - n^2 배열 자르기
+레벨 : Level 2
+링크 : https://school.programmers.co.kr/learn/courses/30/lessons/87390
+*/
+
+// ANCHOR 2025.10.03 풀이
+function solution2(n, left, right) {
+  let answer = [];
+
+  for (let idx = left; idx <= right; idx++) {
+    const div = parseInt(idx / n);
+    const mod = idx % n;
+
+    answer.push(Math.max(div, mod) + 1);
+  }
+
+  return answer;
+}
+
+// ANCHOR 2024.05.10 풀이
+
+function solution1(n, left, right) {
   let answer = [];
   for (let idx = left; idx <= right; idx++) {
     const y = Math.floor(idx / n);
