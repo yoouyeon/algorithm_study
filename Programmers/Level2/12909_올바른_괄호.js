f4570e437b7c86b29bff20a6510fea7796c13121
yoouyeon <jyeon.yoon59@gmail.com>
2025-10-04 23:42:23 +0900

    💡 프로그래머스 12909 - 올바른 괄호

--- Programmers/Level2/12909_올바른_괄호.js
+++ Programmers/Level2/12909_올바른_괄호.js
@@ -5,7 +5,26 @@
 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/12909
 */
 
-function solution(s) {
+// ANCHOR 2025.10.04 풀이
+function solution2(s) {
+  let stackSize = 0;
+
+  for (const c of s) {
+    if (c === "(") {
+      // 여는 괄호인 경우 스택에 넣는다
+      stackSize++; // 괄호 종류가 하나 뿐이므로 직접 스택에 데이터를 넣을 필요가 없음
+    } else {
+      // 닫는 괄호인 경우 스택에서 뺀다
+      if (stackSize === 0) return false;
+      stackSize--;
+    }
+  }
+
+  return stackSize === 0;
+}
+
+// ANCHOR 2024.12.19 풀이
+function solution1(s) {
   let answer = true;
   let stackSize = 0;
 
