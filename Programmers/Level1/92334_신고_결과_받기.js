6af959dd8222ba492b3007d40eb0957060a5654f
yoouyeon <jyeon.yoon59@gmail.com>
2025-10-06 23:48:04 +0900

    💡 프로그래머스 92334 - 신고 결과 받기

--- Programmers/Level1/92334_신고_결과_받기.js
+++ Programmers/Level1/92334_신고_결과_받기.js
@@ -1,3 +1,42 @@
+/* 
+⭐️ 문제 정보 ⭐️
+문제 : 92334 - 신고 결과 받기
+레벨 : Level 1
+링크 : https://school.programmers.co.kr/learn/courses/30/lessons/92334
+*/
+
+// ANCHOR 2025.10.06 풀이
+function solution(id_list, report, k) {
+  const mailMap = new Map(); // 유저별 메일링 리스트
+  const reportMap = new Map(); // 유저별 신고 횟수
+
+  // map 초기화
+  for (const userId of id_list) {
+    mailMap.set(userId, new Set());
+    reportMap.set(userId, 0);
+  }
+  // 신고 정보 처리
+  for (const r of report) {
+    const [from, to] = r.split(" ");
+    if (mailMap.get(from).has(to)) continue;
+    reportMap.set(to, reportMap.get(to) + 1);
+    mailMap.get(from).add(to);
+  }
+  // 메일 수 결정
+  const answer = new Array(id_list.length).fill(0);
+  for (let idx = 0; idx < id_list.length; idx++) {
+    const userId = id_list[idx];
+    for (const reported of mailMap.get(userId).values()) {
+      if (reportMap.get(reported) >= k) {
+        answer[idx]++;
+      }
+    }
+  }
+
+  return answer;
+}
+
+// ANCHOR 2024.03.14 풀이
 function solution(id_list, report, k) {
   var answer = [];
   const userMap = new Map(); // key: userId, value: 신고한 유저의 Set
