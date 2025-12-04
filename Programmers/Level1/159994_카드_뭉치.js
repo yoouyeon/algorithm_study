aabb3985582c4a9bdc3ff27609d6f26173a28643
yoouyeon <jyeon.yoon59@gmail.com>
2025-12-04 10:45:45 +0900

    💡 프로그래머스 159994 - 카드 뭉치

--- Programmers/Level1/159994_카드_뭉치.js
+++ Programmers/Level1/159994_카드_뭉치.js
@@ -5,7 +5,25 @@
 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/159994
 */
 
-function solution(cards1, cards2, goal) {
+// ANCHOR - 2025.12.04 풀이
+function solution2(cards1, cards2, goal) {
+  let front1 = 0;
+  let front2 = 0;
+  let len1 = cards1.length;
+  let len2 = cards2.length;
+
+  for (let idx = 0; idx < goal.length; idx++) {
+    const target = goal[idx];
+    if (front1 < len1 && cards1[front1] === target) front1++;
+    else if (front2 < len2 && cards2[front2] === target) front2++;
+    else return "No";
+  }
+
+  return "Yes";
+}
+
+// ANCHOR - 2025.10.05 풀이
+function solution1(cards1, cards2, goal) {
   let idx1 = 0;
   let idx2 = 0;
   let goalIdx = 0;
