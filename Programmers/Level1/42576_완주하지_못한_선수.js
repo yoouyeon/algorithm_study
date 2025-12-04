afe17a393e9ac251ee9a72421d7c8cec4e966e93
yoouyeon <jyeon.yoon59@gmail.com>
2025-12-04 11:54:54 +0900

    💡 프로그래머스 42576 - 완주하지 못한 선수

--- Programmers/Level1/42576_완주하지_못한_선수.js
+++ Programmers/Level1/42576_완주하지_못한_선수.js
@@ -5,7 +5,33 @@
 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/42576
 */
 
-function solution(participant, completion) {
+// ANCHOR - 2025.12.04 풀이
+function solution2(participant, completion) {
+  // 참여자 맵 초기화
+  const participantMap = new Map(); // key: 이름, value: 해당하는 선수 수
+  for (const name of participant) {
+    if (participantMap.has(name)) {
+      participantMap.set(name, participantMap.get(name) + 1);
+    } else {
+      participantMap.set(name, 1);
+    }
+  }
+
+  // 완주자 확인
+  for (const name of completion) {
+    participantMap.set(name, participantMap.get(name) - 1);
+  }
+
+  // 완주하지 못한 선수 학인
+  for (const [name, count] of participantMap) {
+    if (count > 0) return name;
+  }
+
+  return ""; // NEVER
+}
+
+// ANCHOR - 2025.10.06 풀이
+function solution1(participant, completion) {
   // 1. completion으로 map 만들기
   const map = new Map();
   for (const person of completion) {
