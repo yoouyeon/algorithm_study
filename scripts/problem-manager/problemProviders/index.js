bd5eb7dc34b4e648fdf739b476ffd185d36bc2e0
yoouyeon <jyeon.yoon59@gmail.com>
2026-02-21 05:23:52 +0900

    ♻️ 사용하지 않을 script 제거

--- scripts/problem-manager/problemProviders/index.js
+++
@@ -1,9 +0,0 @@
-import baekjoonProvider from "./baekjoonProvider.js";
-import programmersProvider from "./programmersProvider.js";
-
-const problemProviders = {
-  acmicpc: baekjoonProvider,
-  programmers: programmersProvider,
-};
-
-export default problemProviders;
