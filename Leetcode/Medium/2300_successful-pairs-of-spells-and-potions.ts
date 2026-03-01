1f7951daa34e5c0e7f82899d7c263ddd806e99df
yoouyeon <jyeon.yoon59@gmail.com>
2026-03-02 00:35:42 +0900

    💡 리트코드 2300 - Successful Pairs of Spells and Potions

---
+++ Leetcode/Medium/2300_successful-pairs-of-spells-and-potions.ts
@@ -0,0 +1,41 @@
+/*
+⭐️ 문제 정보 ⭐️
+문제 : 2300 - Successful Pairs of Spells and Potions
+레벨 : Medium
+링크 : https://leetcode.com/problems/successful-pairs-of-spells-and-potions
+*/
+
+// ANCHOR 2026.03.02 풀이
+
+function successfulPairs(spells: number[], potions: number[], success: number): number[] {
+  const answer: number[] = [];
+  // spells를 고정시켜두고, 더해서 success 이상이 되는 potion을 찾는 문제.
+  // success를 넘어가는 그 지점 이후로는 모두 정답이 될 것이기 때문에, 이진탐색을 쓰겠습니다.
+  // 이진탐색을 위한 potions 정렬
+  potions.sort((a, b) => a - b);
+
+  // spells 하나하나마다 이진탐색
+  for (const spell of spells) {
+    let left = 0,
+      right = potions.length - 1;
+
+    while (left < right) {
+      const mid = Math.floor((right + left) / 2);
+      const potion = potions[mid];
+      // mid가 큰 경우 -> 범위를 왼쪽으로 옮긴다.
+      if (spell * potion >= success) {
+        right = mid;
+      }
+      // mid가 크지 않은 경우 -> 범위를 오른쪽으로 옮긴다.
+      else {
+        left = mid + 1;
+      }
+    }
+
+    // 이진탐색을 다 돈 뒤의 left가 실제로 조건을 만족한다면 답으로 넣어준다.
+    if (spell * potions[left] >= success) answer.push(potions.length - left);
+    else answer.push(0);
+  }
+
+  return answer;
+}
