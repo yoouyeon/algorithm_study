c836d83fae6bc2116655e7d294f2fca933c37c04
yoouyeon <jyeon.yoon59@gmail.com>
2026-03-22 14:48:45 +0900

    💡 리트코드 389 - Find the Difference
    아스키 코드 값을 이용하는 방법

--- Leetcode/Easy/389_find-the-difference.ts
+++ Leetcode/Easy/389_find-the-difference.ts
@@ -7,12 +7,21 @@
 
 // ANCHOR 2026.03.22 풀이 (10분 소요)
 function findTheDifference(s: string, t: string): string {
-  const sArr = s.split('').sort();
-  const tArr = t.split('').sort();
+  // 풀이 1. 정렬하는 방법
+  // const sArr = s.split('').sort();
+  // const tArr = t.split('').sort();
 
-  for (let idx = 0; idx < sArr.length; idx++) {
-    if (sArr[idx] !== tArr[idx]) return tArr[idx];
-  }
+  // for (let idx = 0; idx < sArr.length; idx++) {
+  //   if (sArr[idx] !== tArr[idx]) return tArr[idx];
+  // }
 
-  return tArr[tArr.length - 1];
+  // return tArr[tArr.length - 1];
+
+  // 풀이 2. 아스키 코드 값을 이용하는 방법
+  let diff = 0;
+
+  for (const c of t) diff += c.charCodeAt(0);
+  for (const c of s) diff -= c.charCodeAt(0);
+
+  return String.fromCharCode(diff);
 }
