c21563fc03b51277a72a094169521d33319d50ea
yoouyeon <jyeon.yoon59@gmail.com>
2026-03-25 10:10:49 +0900

    💡 리트코드 739 - Daily Temperatures
    인덱스만 저장하는 풀이 추가

--- Leetcode/Medium/739_daily-temperatures.ts
+++ Leetcode/Medium/739_daily-temperatures.ts
@@ -6,18 +6,38 @@
 */
 
 // ANCHOR 2026.03.25 풀이 (18분 소요)
-function dailyTemperatures(temperatures: number[]): number[] {
-  const stack: number[][] = []; // [day, temperature]
-  const ans: number[] = new Array(temperatures.length).fill(0);
+{
+  function dailyTemperatures1(temperatures: number[]): number[] {
+    const stack: number[][] = []; // [day, temperature]
+    const ans: number[] = new Array(temperatures.length).fill(0);
 
-  for (let day = 0; day < temperatures.length; day++) {
-    const temperature = temperatures[day];
-    while (stack.length !== 0 && stack[stack.length - 1][1] < temperature) {
-      ans[stack[stack.length - 1][0]] = day - stack[stack.length - 1][0];
-      stack.pop();
+    for (let day = 0; day < temperatures.length; day++) {
+      const temperature = temperatures[day];
+      while (stack.length !== 0 && stack[stack.length - 1][1] < temperature) {
+        ans[stack[stack.length - 1][0]] = day - stack[stack.length - 1][0];
+        stack.pop();
+      }
+      stack.push([day, temperature]);
     }
-    stack.push([day, temperature]);
+
+    return ans;
   }
 
-  return ans;
+  // index만 저장하는 것도 방법
+  function dailyTemperatures2(temperatures: number[]): number[] {
+    const stack: number[] = []; // index only
+    const ans = new Array(temperatures.length).fill(0);
+
+    for (let i = 0; i < temperatures.length; i++) {
+      while (stack.length && temperatures[stack[stack.length - 1]] < temperatures[i]) {
+        const j = stack[stack.length - 1];
+        stack.pop();
+        ans[j] = i - j;
+      }
+      stack.push(i);
+    }
+
+    return ans;
+  }
+  ㅍ;
 }
