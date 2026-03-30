20eb41b0ee11609206b2ab6de515e06e83b6d3cb
yoouyeon <jyeon.yoon59@gmail.com>
2026-03-30 10:36:17 +0900

    💡 리트코드 345 - Reverse Vowels of a String
    set에 대소문자를 모두 넣어 toLowerCase 호출 생략

--- Leetcode/Easy/345_reverse-vowels-of-a-string.ts
+++ Leetcode/Easy/345_reverse-vowels-of-a-string.ts
@@ -8,16 +8,16 @@
 // ANCHOR 2026.03.30 풀이 (13분 소요)
 function reverseVowels(s: string): string {
   const ret = [...s];
-  // 1. vowels를 판별할 set 정의 (소문자)
-  const vowels = new Set(['a', 'e', 'i', 'o', 'u']);
+  // 1. vowels를 판별할 set 정의 (대소문자 모두 포함)
+  const vowels = new Set('aeiouAEIOU');
   // 2. 투포인터 설정 (양쪽 끝에서 vowel을 만나면 스왑한다.)
   let left = 0;
   let right = s.length - 1;
   // 3. 진행!
   while (left < right) {
     // a. vowels가 등장할 때 까지 포인터 이동
-    while (left < right && !vowels.has(s[left].toLowerCase())) left++;
-    while (left < right && !vowels.has(s[right].toLowerCase())) right--;
+    while (left < right && !vowels.has(s[left])) left++;
+    while (left < right && !vowels.has(s[right])) right--;
     // b. 스왑이 불가능하다면 종료
     if (left >= right) break;
     // c. 스왑
