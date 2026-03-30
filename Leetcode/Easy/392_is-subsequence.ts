ca2ce8abdefa01f908637a2e450a93e334f45390
yoouyeon <jyeon.yoon59@gmail.com>
2026-03-30 12:08:11 +0900

    💡 리트코드 392 - Is Subsequence

--- Leetcode/Easy/392_is-subsequence.ts
+++ Leetcode/Easy/392_is-subsequence.ts
@@ -5,23 +5,37 @@
 링크 : https://leetcode.com/problems/is-subsequence/
 */
 
-/*
- * [392] Is Subsequence
- */
-
-function isSubsequence(s: string, t: string): boolean {
-  let sIdx = 0,
-    tIdx = 0;
-  if (s.length === 0) return true;
-  if (t.length === 0) return false;
-  while (sIdx < s.length && tIdx < t.length) {
-    const sChar = s[sIdx];
-    while (tIdx < t.length && t[tIdx] !== sChar) {
-      tIdx++;
+// ANCHOR 2026.03.30 풀이 (4분 소요)
+{
+  function isSubsequence(s: string, t: string): boolean {
+    // s가 t의 subsequence인지를 확인해야 함.
+    // s에 등장하는 문자 순서대로 t에 모두 등장하는지만 확인하면 된다.
+    let tidx = 0;
+    for (const c of s) {
+      while (tidx < t.length && c !== t[tidx]) tidx++;
+      if (tidx >= t.length) return false;
+      tidx++;
     }
-    if (tIdx >= t.length) return false;
-    tIdx++;
-    sIdx++;
+    return true;
   }
-  return sIdx === s.length;
+}
+
+// ANCHOR 2025.08.12 풀이
+{
+  function isSubsequence(s: string, t: string): boolean {
+    let sIdx = 0,
+      tIdx = 0;
+    if (s.length === 0) return true;
+    if (t.length === 0) return false;
+    while (sIdx < s.length && tIdx < t.length) {
+      const sChar = s[sIdx];
+      while (tIdx < t.length && t[tIdx] !== sChar) {
+        tIdx++;
+      }
+      if (tIdx >= t.length) return false;
+      tIdx++;
+      sIdx++;
+    }
+    return sIdx === s.length;
+  }
 }
