0f5de18e72754b607c383d9357587608a548a6c5
yoouyeon <jyeon.yoon59@gmail.com>
2026-04-01 17:12:45 +0900

    💡 프로그래머스 1845 - 폰켓몬

--- Programmers/Level1/1845_폰켓몬.js
+++ Programmers/Level1/1845_폰켓몬.js
@@ -1,3 +1,20 @@
+/*
+⭐️ 문제 정보 ⭐️
+문제 : 1845 - 폰켓몬
+레벨 : Level 1
+링크 : https://school.programmers.co.kr/learn/courses/30/lessons/1845
+*/
+
+// ANCHOR 2026.04.01 풀이 (8분 소요)
+function solution(nums) {
+  // N/2가 폰켓몬 종류보다 크다면 특정 폰켓몬을 1마리 이상 가져가야 한다 => 폰켓몬 종류만큼 가져갈 수 있음
+  // N/2가 폰켓몬 종류보다 크지 않다면 그냥 N/2 종류를 1마리씩 가져가면 된다 => N/2종류 가져갈 수 있음
+  const ponkedex = new Set(nums);
+  const n = nums.length;
+  return Math.floor(n / 2) > ponkedex.size ? ponkedex.size : Math.floor(n / 2);
+}
+
+// ANCHOR 2023.11.01 풀이
 function solution(nums) {
   const ponketmonSet = new Set(nums);
   const half = nums.length / 2;
