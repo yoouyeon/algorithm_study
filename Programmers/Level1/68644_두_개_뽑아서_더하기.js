de73ffcac671d5851d78637d088621d1401c6539
yoouyeon <jyeon.yoon59@gmail.com>
2026-04-01 16:44:42 +0900

    💡 프로그래머스 68644 - 두 개 뽑아서 더하기

--- Programmers/Level1/68644_두_개_뽑아서_더하기.js
+++ Programmers/Level1/68644_두_개_뽑아서_더하기.js
@@ -1,17 +1,34 @@
-/* 
+/*
 ⭐️ 문제 정보 ⭐️
 문제 : 68644 - 두 개 뽑아서 더하기
 레벨 : Level 1
 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/68644
 */
 
-function solution(numbers) {
-  let answerSet = new Set();
-
-  for (let i = 0; i < numbers.length - 1; i++) {
-    for (let j = i + 1; j < numbers.length; j++) {
-      answerSet.add(numbers[i] + numbers[j]);
+// ANCHOR 2026.04.01 풀이 (4분 소요)
+{
+  function solution(numbers) {
+    // 합이 같은 것은 하나만 담는다.
+    const set = new Set();
+    for (let i = 0; i < numbers.length - 1; i++) {
+      for (let j = i + 1; j < numbers.length; j++) {
+        set.add(numbers[i] + numbers[j]);
+      }
     }
+    return [...set].sort((a, b) => a - b);
   }
-  return [...answerSet].sort((a, b) => a - b);
+}
+
+// ANCHOR 2025.10.01 풀이
+{
+  function solution(numbers) {
+    let answerSet = new Set();
+
+    for (let i = 0; i < numbers.length - 1; i++) {
+      for (let j = i + 1; j < numbers.length; j++) {
+        answerSet.add(numbers[i] + numbers[j]);
+      }
+    }
+    return [...answerSet].sort((a, b) => a - b);
+  }
 }
