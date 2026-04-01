f1204057b8c4ba3598f54fdcfb8cf9515bb886b6
yoouyeon <jyeon.yoon59@gmail.com>
2026-04-01 19:58:49 +0900

    💡 프로그래머스 77484 - 로또의 최고 순위와 최저 순위

--- Programmers/Level1/77484_로또의_최고_순위와_최저_순위.js
+++ Programmers/Level1/77484_로또의_최고_순위와_최저_순위.js
@@ -1,3 +1,31 @@
+/*
+⭐️ 문제 정보 ⭐️
+문제 : 77484 - 로또의 최고 순위와 최저 순위
+레벨 : Level 1
+링크 : https://school.programmers.co.kr/learn/courses/30/lessons/77484
+*/
+
+// ANCHOR 2026.04.01 풀이 (9분 소요)
+function solution(lottos, win_nums) {
+  const rank = [6, 6, 5, 4, 3, 2, 1]; // i개 맞추면 rank[i]등
+  const winSet = new Set(win_nums);
+
+  let zero = 0;
+  let count = 0;
+  for (const lotto of lottos) {
+    if (winSet.has(lotto)) {
+      count++;
+      winSet.delete(lotto);
+    } else if (lotto === 0) {
+      zero++;
+    }
+  }
+
+  let answer = [rank[count + zero], rank[count]];
+  return answer;
+}
+
+// ANCHOR 2024.03.09 문제 풀이
 function solution(lottos, win_nums) {
   let answer = [];
   const ranking = [6, 6, 5, 4, 3, 2, 1]; // idx 만큼 맞추면 몇등인지
