0e1616b47a413440c34753e974d3542a2a5c242c
yoouyeon <jyeon.yoon59@gmail.com>
2026-04-02 17:22:05 +0900

    💡 프로그래머스 120861 - 캐릭터의 좌표
    피드백 반영
    - Map 대신 객체 활용
    - 범위 비교에 Math.abs 활용
    - 음수 표현에 곱셈 대신 그냥 - 기호 쓰기

--- Programmers/Level0/120861_캐릭터의_좌표.js
+++ Programmers/Level0/120861_캐릭터의_좌표.js
@@ -7,12 +7,8 @@
 
 // ANCHOR 2026.04.02 풀이 (16분 소요)
 function solution(keyinput, board) {
-  const map = new Map([
-    ['up', [0, 1]],
-    ['down', [0, -1]],
-    ['left', [-1, 0]],
-    ['right', [1, 0]],
-  ]);
+  // map을 사용해도 괜찮지만 (원래 그렇게 풀었었음) 키가 고정적으로 4개이기 때문에 객체가 더 간결하다.
+  const moves = { up: [0, 1], down: [0, -1], left: [-1, 0], right: [1, 0] };
 
   // 머쓱이 캐릭터 위치
   let y = 0;
@@ -22,10 +18,13 @@ function solution(keyinput, board) {
   const [xmax, ymax] = [Math.floor(board[0] / 2), Math.floor(board[1] / 2)];
 
   keyinput.forEach((input) => {
-    const move = map.get(input);
+    const move = moves[input];
     const ny = y + move[1];
     const nx = x + move[0];
-    if (ny < ymax * -1 || ny > ymax || nx < xmax * -1 || nx > xmax) return;
+    // 피드백 1. ymax * -1보다 -ymax가 더 관용적인 쓰임
+    // 피드백 2. Math.abs()를 사용하면 더 간결하게 표현 가능
+    // if (ny < ymax * -1 || ny > ymax || nx < xmax * -1 || nx > xmax) return;
+    if (Math.abs(ny) > ymax || Math.abs(nx) > xmax) return;
     y = ny;
     x = nx;
   });
